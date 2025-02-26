73bbdbf826e9e3c11fb81cf8c14c158824394d5d
Sergei Usachev <usachev97@gmail.com>
2025-02-26 22:42:11 +0100

    add hover for header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,29 +3,44 @@ import Link from 'next/link'
 
 export const Header: React.FC = () => {
     return (
-        <div className="bg-white flex flex-col gap-4 justify-center items-center w-full pt-4 pb-6">
+        <div className="bg-white flex flex-col space-y-4 justify-center items-center w-full pt-4 pb-6">
             <Image src="/logotype.png" width={290} height={88} alt="Austris candles logo" />
-            <nav className="flex flex-row gap-8 items-center justify-between">
-                <Link href="/candles" className="text-xs uppercase">
+            <nav className="flex flex-row space-x-3 items-center justify-between sm:space-x-8 ">
+                <Link
+                    href="/candles"
+                    className="text-xs text-black uppercase transition duration-300 ease-in-out hover:text-black/70"
+                >
                     Candles
                 </Link>
-                <Link href="/about" className="text-xs uppercase">
+                <Link
+                    href="/about"
+                    className="text-xs text-black uppercase transition duration-300 ease-in-out hover:text-black/70"
+                >
                     About us
                 </Link>
-                <Link href="/wholesale" className="text-xs uppercase">
+                <Link
+                    href="/wholesale"
+                    className="text-xs text-black uppercase transition duration-300 ease-in-out hover:text-black/70"
+                >
                     Wholesale
                 </Link>
                 <Link
                     href="https://flowwow.com/en-en/shop/austris-handmade-candles/"
                     target="_blank"
-                    className="text-xs uppercase"
+                    className="text-xs text-black uppercase transition duration-300 ease-in-out hover:text-black/70"
                 >
                     Flowwow
                 </Link>
-                <Link href="/contacts" className="text-xs uppercase">
+                <Link
+                    href="/contacts"
+                    className="text-xs text-black uppercase transition duration-300 ease-in-out hover:text-black/70"
+                >
                     Contact us
                 </Link>
-                <Link href="/cart" className="text-xs uppercase">
+                <Link
+                    href="/cart"
+                    className="text-xs text-black uppercase transition duration-300 ease-in-out hover:text-black/70"
+                >
                     Cart
                 </Link>
             </nav>
